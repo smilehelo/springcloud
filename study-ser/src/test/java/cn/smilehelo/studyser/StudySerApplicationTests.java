d90b80a2c481c5be93434f172e7242466dcfdd55
smile_helo <smile_helo@163.com>
2019-11-11 16:58:40 +0800

    no message

--- study-ser/src/test/java/cn/smilehelo/studyser/StudySerApplicationTests.java
+++ study-ser/src/test/java/cn/smilehelo/studyser/StudySerApplicationTests.java
@@ -2,21 +2,18 @@ package cn.smilehelo.studyser;
 
 import cn.smilehelo.studyser.dao.TestDao;
 import cn.smilehelo.studyser.entity.TestEntity;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
 @SpringBootTest
 public class StudySerApplicationTests {
 
     @Autowired
     private TestDao testDao;
 
-    @Test
+
+    
     public void test() {
         TestEntity entity = new TestEntity();
         entity.setName("20181218");
